f32d016ae96c64746828178858c7ac6c2d71d6f0
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2024-02-08 17:07:26 -0300

    :wrench: tpoc rarities

--- src/riot-assets/models-tpoc.ts
+++ src/riot-assets/models-tpoc.ts
@@ -1,5 +1,28 @@
 import {RIOT_LOR_RARITY_REF} from "./models-globals";
 
+// temp1 é o en_us.json do assets/tpoc
+// [...new Set([...temp1.items, ...temp1.powers, ...temp1.relics].map(i => i.rarityRef))]
+export enum RIOT_LOR_TPOC_RARITY_REF {
+  COMMON = 'Common',
+  EPIC = 'Epic',
+  LEGENDARY = 'Legendary',
+  RARE = 'Rare',
+  SPECIAL = 'Special',
+}
+
+export type RiotLorTpocRarityRef =
+  | RIOT_LOR_TPOC_RARITY_REF.COMMON
+  | RIOT_LOR_TPOC_RARITY_REF.EPIC
+  | RIOT_LOR_TPOC_RARITY_REF.LEGENDARY
+  | RIOT_LOR_TPOC_RARITY_REF.RARE
+  | RIOT_LOR_TPOC_RARITY_REF.SPECIAL;
+
+export function isRiotLorTpocRarityRef(variable: any): boolean {
+  return Object.values(RIOT_LOR_TPOC_RARITY_REF)
+    .map((i) => `${i}`)
+    .includes(`${variable}`);
+}
+
 export interface RiotLoRTPoCItem {
   assetAbsolutePath: string
   assetFullAbsolutePath: string
@@ -8,7 +31,7 @@ export interface RiotLoRTPoCItem {
   name: string
   itemCode: string
   rarity: string
-  rarityRef: RIOT_LOR_RARITY_REF
+  rarityRef: RIOT_LOR_TPOC_RARITY_REF
 }
 
 export function isRiotLoRTPoCItem(object: any): boolean {
@@ -30,7 +53,7 @@ export interface RiotLoRTPoCRelic {
   name: string
   relicCode: string
   rarity: string
-  rarityRef: RIOT_LOR_RARITY_REF
+  rarityRef: RIOT_LOR_TPOC_RARITY_REF
 }
 
 export function isRiotLoRTPoCRelic(object: any): boolean {
@@ -52,7 +75,7 @@ export interface RiotLoRTPoCPower {
   name: string
   powerCode: string
   rarity: string
-  rarityRef: RIOT_LOR_RARITY_REF
+  rarityRef: RIOT_LOR_TPOC_RARITY_REF
 }
 
 export function isRiotLoRTPoCPower(object: any): boolean {
